2071d3e41d6f25ff0cca661d7c6d15c606790c65
Estelle He <wh103@duke.edu>
2018-11-07 12:19:17 -0500

    working script

--- compute_hash.py
+++ compute_hash.py
@@ -1,12 +1,11 @@
 import xxhash
 from multiprocessing import Process, Pool
 import os
+import time
 
-hash_dict = {}
 hashf = xxhash.xxh64
 files = []
 dir = "./"
-hash_list = []
 
 def compute_hash(file):
 	def file_as_blockiter(file, blocksize=4096):
@@ -30,21 +29,23 @@ def multithread_checksum():
 	for i in range(0, len(files), 20):
 		results = p.map_async(compute_hash, files[i:min(i+20, len(files))])
 		results = results.get()
-		for result in results:
-            hash_list.append(result)
-		counter += len(results)
-		print('FINISHED {}/{}	ESTIMATED TIME REMAINING: {}'.format(counter, len(files), (time.time()-start_time)*(len(files)-counter)/counter))
+        for result in results:
+            hash_list.append("{}, {}\n".format(result[1], result[0]))
+	p.close()
+    #print('FINISHED {}/{}	ESTIMATED TIME REMAINING: {}'.format(counter, len(files), (time.time()-start_time)*(len(files)-counter)/counter))
 
 
 if __name__ == '__main__':
     for txt in os.listdir(dir):
+        start_time = time.time()
         if txt.startswith("path_hash_") and not txt.endswith("_finished.txt"):
-            files.clear()
-            hash_list.clear()
+            files = []
+            hash_list = []
             with open(os.path.join(dir, txt), 'rb') as f:
-                files.extend(f.readlines())
+                files.extend([line.replace("\n", "") for line in f.readlines()])
             multithread_checksum()
+            print ("finished {}".format(txt))
             with open(os.path.join(dir, txt.replace(".txt", "_finished.txt")), 'w+') as f:
                 for hash in hash_list:
-                    f.write(hash + "\n")
-            os.remove(os.path.join(dir, txt))
+                    f.write(hash)
+            #os.remove(os.path.join(dir, txt))
--- get_all_path.py
+++ get_all_path.py
@@ -4,7 +4,7 @@ import sys
 
 files = []
 num_per_f = 100
-dir = "./"
+result_dir = "./"
 
 def get_all_files(path):
 	for entry in scandir.walk(path):
@@ -13,11 +13,18 @@ def get_all_files(path):
 
 
 if __name__ == '__main__':
-    dir = sys.argv[1]
+	dir = sys.argv[1]
 
-    get_all_files(dir)
+	get_all_files(dir)
 
-    for i in range(0, len(files)/num_per_f+1):
-        with open(os.path.join(dir, "path_hash_{}.txt".format(i)), 'w+') as f:
-            for file in files[i*num_per_f : (i+1)*num_per_f-1]:
-                f.write(file + "\n")
+	if (len(sys.argv) > 2):
+		get_all_files(sys.argv[2])
+
+	print("finish traversing path")
+
+	for i in range(0, len(files)/num_per_f+1):
+		with open(os.path.join(result_dir, "path_hash_{}.txt".format(i)), 'w+') as f:
+			print i*num_per_f, (i+1)*num_per_f-1
+			print files
+			for file in files[i*num_per_f : (i+1)*num_per_f]:
+				f.write(file + "\n")
