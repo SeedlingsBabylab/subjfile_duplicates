86b6b3293c4e15571cbf8577abf697787c0b2623
Estelle He <wh103@duke.edu>
2018-11-26 12:58:07 -0500

    compare ready

--- compare_hash.py
+++ compare_hash.py
@@ -3,6 +3,7 @@ import sys
 
 
 hash_dict = {}
+result_dir = "./"
 
 def read_file(path):
     with open(path, 'rb') as f:
@@ -15,9 +16,9 @@ def read_file(path):
 
 def process_dir(dir1):
     for folder in os.listdir(dir1):
-		if os.path.isdir(os.path.join(dir1, folder)):
-			dir = os.path.join(dir1, folder)
-			for txt in os.listdir(dir):
+        if os.path.isdir(os.path.join(dir1, folder)):
+            dir = os.path.join(dir1, folder)
+            for txt in os.listdir(dir):
                 if txt.startswith("path_hash_") and txt.endswith("_finished.txt"):
                     read_file(os.path.join(dir, txt))
 
@@ -29,8 +30,11 @@ if __name__ == '__main__':
     dir1 = sys.argv[1]
     process_dir(dir1)
 
-    if (len(sys.argv) > 1):
+    if (len(sys.argv) > 2):
         dir2 = sys.argv[2]
         process_dir(dir2)
 
-    dup_list = [hash_dict[key] for key in hash_dict.keys() if len(hash_dict[key]) > 1]
+dup_list = [", ".join(hash_dict[key]) for key in hash_dict.keys() if len(hash_dict[key]) > 1]
+with open(os.path.join(result_dir, "dup_result.txt"), 'w+') as f:
+    for dup in dup_list:
+        f.write(dup + "\n")
